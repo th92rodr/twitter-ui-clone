61edfe7e76dbc78f642adfb1b7d271cf28af396e
th92rodr <thiago.clrodrigues@gmail.com>
2020-07-18 20:39:16 -0300

    create Main component

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import ProfilePage from '../ProfilePage';
+
+import {
+  Container,
+  Header,
+  BackIcon,
+  ProfileInfo,
+  BottomMenu,
+  HomeIcon,
+  SearchIcon,
+  BellIcon,
+  EmailIcon,
+} from './styles';
+
+const Main: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <button>
+          <BackIcon />
+        </button>
+
+        <ProfileInfo>
+          <strong>Thiago Rodrigues</strong>
+          <span>250 Tweets</span>
+        </ProfileInfo>
+      </Header>
+
+      <ProfilePage />
+
+      <BottomMenu>
+        <HomeIcon />
+        <SearchIcon />
+        <BellIcon />
+        <EmailIcon />
+      </BottomMenu>
+    </Container>
+  );
+};
+
+export default Main;
