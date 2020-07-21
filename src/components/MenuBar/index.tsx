d243d63d6b2e62012ed825fcadb0417f9b9220c3
th92rodr <45070690+th92rodr@users.noreply.github.com>
2020-07-21 20:27:36 -0300

    Create MenuBar component

---
+++ src/components/MenuBar/index.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+import Button from '../Button';
+import {
+  Container,
+  Topside,
+  Logo,
+  MenuButton,
+  HomeIcon,
+  BellIcon,
+  EmailIcon,
+  FavoriteIcon,
+  ProfileIcon,
+  ExitIcon,
+  Botside,
+  Avatar,
+  ProfileData,
+} from './styles';
+
+const Layout: React.FC = () => {
+  return (
+    <Container>
+      <Topside>
+        <Logo />
+
+        <MenuButton>
+          <HomeIcon />
+          <span>Página Inicial</span>
+        </MenuButton>
+
+        <MenuButton>
+          <BellIcon />
+          <span>Notificações</span>
+        </MenuButton>
+
+        <MenuButton>
+          <EmailIcon />
+          <span>Mensagens</span>
+        </MenuButton>
+
+        <MenuButton>
+          <FavoriteIcon />
+          <span>Favoridados</span>
+        </MenuButton>
+
+        <MenuButton className='active'>
+          <ProfileIcon />
+          <span>Perfil</span>
+        </MenuButton>
+
+        <Button>
+          <span>Tweetar</span>
+        </Button>
+      </Topside>
+
+      <Botside>
+        <Avatar />
+
+        <ProfileData>
+          <strong>Thiago Rodrigues</strong>
+          <span>@th92rodr</span>
+        </ProfileData>
+
+        <ExitIcon />
+      </Botside>
+    </Container>
+  );
+};
+
+export default Layout;
